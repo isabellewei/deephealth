a439db6341939c9f8c5f8e12e3fe45b82c8ecec2
Jerry Li <jerry@jerryli.ca>
2017-02-18 12:49:13 -0500

    parsed.csv now includes encounter_id

--- data/parsecsv.py
+++ data/parsecsv.py
@@ -2,16 +2,17 @@
 import pandas as pd
 
 df = pd.read_csv('diabetes-training.csv')
-del df['encounter_id']
+# del df['encounter_id']
 nan = df['weight'][0]
 cols = list(df)
-numerical = set(['patient_nbr', 'admission_type_id', 'discharge_disposition_id', 'admission_source_id', 'time_in_hospital', 'num_lab_procedures',
+numerical = set(['encounter_id', 'patient_nbr', 'admission_type_id', 'discharge_disposition_id', 'admission_source_id', 'time_in_hospital', 'num_lab_procedures',
                  'num_procedures', 'num_medications', 'number_outpatient', 'number_emergency', 'number_inpatient', 'number_diagnoses'])
 text_cols = [x for x in cols if x not in numerical]
 for cat in text_cols:
     options = df[cat].unique()
     mapping = dict(zip(options, range(len(options))))
     mapping[nan] = -1
+    print cat
     df = df.replace({cat: mapping})
 
 df = df.fillna(-1)
