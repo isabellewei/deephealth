d2306f7f94d3d71fd98f5b58496bb7d34a9e3547
MalusGreen <kevin97illu@gmail.com>
2017-02-17 21:33:37 -0500

    Fixed up a few issues.

--- stack/src/components/Layout.js
+++ stack/src/components/Layout.js
@@ -6,10 +6,10 @@ import { Link } from 'react-router';
 export default class Layout extends React.Component {
   render() {
     return (
-		<div>Text located in Layout.js</div>
-		//Place where every other page component goes.
-        <div className="app-content">{this.props.children}</div>
-        
+        <div className="app-content">
+			<div>Text from Layout.js.</div>
+        	{this.props.children}
+        </div>
     );
   }
 }
