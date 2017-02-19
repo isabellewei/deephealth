46f97ff65906675f3c8b6e2ac0d4b1c73eb29616
MalusGreen <kevin97illu@gmail.com>
2017-02-18 23:21:57 -0500

    changes to neural nets.

--- data/gary/neural.py
+++ data/gary/neural.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
-df = pd.read_csv("parsed.csv")
+df = pd.read_csv("../parsed.csv")
 y1 = df["admission_type_id"].values
 y2 = df["discharge_disposition_id"].values
 columns = list(df)[1:4] + list(df)[7:49]
@@ -27,8 +27,17 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-mlp = MLPClassifier(hidden_layer_sizes=(30,30,30), batch_size=200)
+mlp = MLPClassifier(hidden_layer_sizes=(50,25), warm_start=True,
+                    batch_size=20, solver='sgd', activation='relu', alpha=0.001,
+                    learning_rate='adaptive', learning_rate_init=0.01,
+                    max_iter=1000
+                    )
+#train 1
+mlp.fit(X_train,y_train)
+predictions = mlp.predict(X_test)
+print(classification_report(y_test, predictions))
 
+#train 2
 mlp.fit(X_train,y_train)
 predictions = mlp.predict(X_test)
 print(classification_report(y_test, predictions))
